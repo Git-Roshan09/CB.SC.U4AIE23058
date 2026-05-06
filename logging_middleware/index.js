375a25be61167f14f0acd48cee8523d3b1c041cc
Roshan <88413854+Twinn-github09@users.noreply.github.com>
2026-05-06 14:26:58 +0530

    feat: added logging middleware

---
+++ logging_middleware/index.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+
+const LOG_API_URL = "http://20.207.122.201/evaluation-service/logs";
+
+async function Log(stack, level, packageName, message) {
+  try {
+    
+    const body = {
+      stack: stack,
+      level: level,
+      package: packageName,
+      message: message,
+    };
+
+    
+    const response = await axios.post(LOG_API_URL, body, {
+      headers: {
+        Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    return response.data;
+  } 
+  catch (error) {
+    console.error("[LogMiddleware] Failed to send log:", error.message);
+  }
+}
+
+module.exports = { Log };
